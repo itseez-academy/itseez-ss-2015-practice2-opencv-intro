0bb951a1f9602df6f593ad2c8cf0f7d3c6bbdaf8
valentina-kustikova <valentina.kustikova@gmail.com>
2015-08-13 15:26:03 +0300

    On/Off buttons sample.

--- sample_template/application.cpp
+++ sample_template/application.cpp
@@ -5,7 +5,8 @@
 
 using namespace cv;
 
-int Application::parseArguments(int argc, const char **argv, Application::Parameters &params)
+int Application::parseArguments(int argc, const char **argv, 
+        Application::Parameters &params)
 {
     if (argc < 2)
     {
@@ -37,9 +38,39 @@ int Application::processFrame(const Mat& src, Mat& dst)
     return 0;
 }
 
-int Application::showFrame(const std::string &caption, const Mat& src, const Mat& dst)
+int Application::drawButtons(Mat &display)
 {
-    if (src.rows != dst.rows || src.cols != dst.cols)
+    guiState.onButtonPlace = Rect(20, display.rows - 60, 120, 40);
+    guiState.offButtonPlace = Rect(160, display.rows - 60, 120, 40);
+    rectangle(display, guiState.onButtonPlace, 
+              Scalar(128, 128, 128), CV_FILLED);
+    rectangle(display, guiState.offButtonPlace, 
+              Scalar(128, 128, 128), CV_FILLED);
+
+    putText(display, "on", 
+        Point(guiState.onButtonPlace.x + guiState.onButtonPlace.width / 2 - 15,
+              guiState.onButtonPlace.y + guiState.onButtonPlace.height / 2 + 10),
+        FONT_HERSHEY_SIMPLEX, 1.0, Scalar(0, 0, 0), 2);
+    putText(display, "off", 
+        Point(guiState.offButtonPlace.x + guiState.offButtonPlace.width / 2 - 20,
+              guiState.offButtonPlace.y + guiState.offButtonPlace.height / 2 + 10),
+        FONT_HERSHEY_SIMPLEX, 1.0, Scalar(0, 0, 0), 2);
+
+    return 0;
+}
+
+int Application::showFrame(const std::string &caption, 
+        const Mat& src, Mat& dst)
+{
+    if (guiState.state == OffFilter)
+    {
+        src.copyTo(dst);
+    }
+    else if (guiState.state == OnFilter)
+    {
+        processFrame(src, dst);
+    }
+    else
     {
         return 1;
     }
@@ -48,11 +79,54 @@ int Application::showFrame(const std::string &caption, const Mat& src, const Mat
     Mat srcRoi = display(Rect(0, 0, src.cols, src.rows));
     src.copyTo(srcRoi);
     Mat dstRoi = display(Rect(src.cols, 0, dst.cols, dst.rows));
-    dst.copyTo(dstRoi);
-
-    namedWindow(caption);
+    dst.copyTo(dstRoi);       
+    
+    drawButtons(display);
+    
+    namedWindow(caption);  
     imshow(caption, display);
+    setMouseCallback(caption, onButtonsOnOffClick, &guiState);
     char key = waitKey(1);
 
     return key;
+}
+
+void onButtonsOnOffClick(int eventId, int x, int y, int flags, void *userData)
+{
+    if (eventId != EVENT_LBUTTONDOWN)
+    {
+        return;
+    }
+    Application::GUIElementsState *elems = 
+        (Application::GUIElementsState *)userData;
+    if (onButtonClicked(elems->onButtonPlace, x, y))
+    {
+        elems->state = Application::WindowState::OnFilter;
+        return;
+    }
+    if (offButtonClicked(elems->offButtonPlace, x, y))
+    {
+        elems->state = Application::WindowState::OffFilter;
+        return;
+    }
+}
+
+bool onButtonClicked(cv::Rect onButtonPlace, int x, int y)
+{
+    if (x < onButtonPlace.x || x > onButtonPlace.x + onButtonPlace.width ||
+        y < onButtonPlace.y || y > onButtonPlace.y + onButtonPlace.height)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool offButtonClicked(cv::Rect offButtonPlace, int x, int y)
+{
+    if (x < offButtonPlace.x || x > offButtonPlace.x + offButtonPlace.width ||
+        y < offButtonPlace.y || y > offButtonPlace.y + offButtonPlace.height)
+    {
+        return false;
+    }
+    return true;
 }
--- sample_template/application.hpp
+++ sample_template/application.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 #include <opencv2/core/core.hpp>
 
 #include "processing.hpp"
@@ -8,16 +9,39 @@
 class Application
 {
  public:
+    enum WindowState
+    {
+        OnFilter,
+        OffFilter
+    };
     struct Parameters
     {
         std::string imgFileName;
     };
+    struct GUIElementsState
+    {
+        WindowState state;
+        cv::Rect onButtonPlace;
+        cv::Rect offButtonPlace;
+    };
     int parseArguments(int argc, const char **argv, Parameters &params);
-
     int getFrame(const std::string &fileName, cv::Mat& src);
     int processFrame(const cv::Mat& src, cv::Mat& dst);
-    int showFrame(const std::string &caption, const cv::Mat& src, const cv::Mat& dst);
+    int showFrame(const std::string &caption, 
+                  const cv::Mat& src, cv::Mat& dst);
+    friend void onButtonsOnOffClick(int eventId, int x, int y, 
+                                    int flags, void *userData);
+    Application() 
+    { 
+        guiState.state = OnFilter;
+    };
 
  private:
     Processing processor;
+    GUIElementsState guiState;
+
+    int drawButtons(cv::Mat &display);
+    
+    friend bool onButtonClicked(cv::Rect onButtonPlace, int x, int y);
+    friend bool offButtonClicked(cv::Rect offButtonPlace, int x, int y);
 };
